c146f4bbf94101d87b707ed8de3d359d1186d100
Nilam <shahnilam812@gmail.com>
2023-11-09 21:22:08 +0530

    Initial Commit

---
+++ Gestures/GesturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GesturesApp.swift
+//  Gestures
+//
+//  Created by Nilam Shah on 09/11/23.
+//
+
+import SwiftUI
+
+@main
+struct GesturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
